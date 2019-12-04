a16aca075f24cd2f59b99ff9f5fa0718791ba769
kei-ebihara <kei.ebihara.hikarituushin@gmail.com>
2019-12-04 18:24:05 +0900

    フラッシュ作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,22 @@
-class PostsController < ApplicationController
+  class PostsController < ApplicationController
 
-  def new
-    @post = Post.new
+    def new
+      @post = Post.new # フォーム用の空のインスタンスを生成する。
+    end
+
+    def create
+      @post = Post.new(post_params) # ストロングパラメータを引数に
+      @post.save # saveをしてデータベースに保存する。
+      redirect_to @post, notice: '投稿を保存しました' # showページにリダイレクト
+    end
+
+    def show
+      @post = Post.find(params[:id])
+    end
+
+    private
+
+    def post_params # ストロングパラメータを定義する
+      params.require(:post).permit(:caption, :new_image)
+    end
   end
-
-  def create
-    @post = Post.new(post_params)
-    @post.save
-    redirect_to @post
-  end
-
-  def show
-    @post = Post.find(params[:id])
-  end
-
-  private
-
-  def post_params
-    params.require(:post).permit(:caption, :new_image)
-  end
-end
